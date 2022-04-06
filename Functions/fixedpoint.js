fd198ef66368ac01b79c71442e2f40915f27badf
bob <Drase0117@gmail.com>
2022-04-06 11:45:29 -0500

    fixing fixedpoint

--- Functions/fixedpoint.js
+++ Functions/fixedpoint.js
@@ -1,17 +1,32 @@
 const mathjs = require('mathjs')
 module.exports = (f, g, x0, tol, nMax) => {
+    const iterations = [];
+    x1 = mathjs.evaluate(g, {x:x0});
+
+    
+    fx1 = mathjs.evaluate(f, {x:x0});
+    error = null;
+    iterations.push({x0,x1,fx1,error});
+    error = Math.abs(x1-x0);
+    x0=x1;
     for (i=0;i<nMax;i++){
+        
         x1 = mathjs.evaluate(g, {x:x0});
-
-        console.log(i," ",x1);
-
+        fx1 = mathjs.evaluate(f, {x:x0});
+        iterations.push({x0,x1,fx1,error});
+        error = Math.abs(x1-x0);
+        
         if (x1===mathjs.evaluate(f, {x:x1})){
             break;
         }
-        if(Math.abs((x1-x0))<=tol){
+        if(error<=tol){
+            x0=x1;
+            x1 = mathjs.evaluate(g, {x:x0});
+            fx1 = mathjs.evaluate(f, {x:x0});
+            iterations.push({x0,x1,fx1,error});
             break;
         }
     x0=x1;
     }
-    
+    return {x:x0,iterations};
 }
