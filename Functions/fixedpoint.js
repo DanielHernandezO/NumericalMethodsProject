2f13a6ceb36dcd271f402673dd6f1366e91f9fc5
bob <Drase0117@gmail.com>
2022-04-06 11:53:48 -0500

    fixing fixedpoint

--- Functions/fixedpoint.js
+++ Functions/fixedpoint.js
@@ -1,32 +1,32 @@
 const mathjs = require('mathjs')
 module.exports = (f, g, x0, tol, nMax) => {
     const iterations = [];
-    x1 = mathjs.evaluate(g, {x:x0});
+    gx1 = mathjs.evaluate(g, {x:x0});
 
     
     fx1 = mathjs.evaluate(f, {x:x0});
     error = null;
-    iterations.push({x0,x1,fx1,error});
-    error = Math.abs(x1-x0);
-    x0=x1;
+    iterations.push({x0,gx1,fx1,error});
+    error = Math.abs(gx1-x0);
+    x0=gx1;
     for (i=0;i<nMax;i++){
         
-        x1 = mathjs.evaluate(g, {x:x0});
+        gx1 = mathjs.evaluate(g, {x:x0});
         fx1 = mathjs.evaluate(f, {x:x0});
-        iterations.push({x0,x1,fx1,error});
-        error = Math.abs(x1-x0);
+        iterations.push({x0,gx1,fx1,error});
+        error = Math.abs(gx1-x0);
         
-        if (x1===mathjs.evaluate(f, {x:x1})){
+        if (gx1===mathjs.evaluate(f, {x:gx1})){
             break;
         }
         if(error<=tol){
-            x0=x1;
+            x0=gx1;
             x1 = mathjs.evaluate(g, {x:x0});
             fx1 = mathjs.evaluate(f, {x:x0});
-            iterations.push({x0,x1,fx1,error});
+            iterations.push({x0,gx1,fx1,error});
             break;
         }
-    x0=x1;
+    x0=gx1;
     }
     return {x:x0,iterations};
 }
